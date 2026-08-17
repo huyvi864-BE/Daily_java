f4a307b9b65f751e1f8c65be45d305aaf49a3946
Vi Quang Huy <huyvq.b25cc084@stu.ptit.edu.vn>
2026-08-17 22:24:37 +0700

    Time: 1 ms (96.35%), Space: 43.9 MB (62.59%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            if (candy > maxCandies) {
+                maxCandies = candy;
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            if (candy + extraCandies >= maxCandies) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        
+        return result;
+    }
+}
