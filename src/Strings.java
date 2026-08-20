583a5f5eefa18fb2e5bbeffa6ac78385c0f4dbe9
Vi Quang Huy <huyvq.b25cc084@stu.ptit.edu.vn>
2026-08-20 23:27:45 +0700

    feat: practice Java two string

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name1 = sc.nextLine();
+        String name2 = sc.nextLine();
+        if (name1.equals(name2)){
+            System.out.println("two people have the same name");
+        } else{
+            System.out.println("two people don't have the same name");
+        }
+    }
+}
