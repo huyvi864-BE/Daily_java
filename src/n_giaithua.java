d2ad3b521ca70fde76821655dd8db41154181947
Vi Quang Huy <huyvq.b25cc084@stu.ptit.edu.vn>
2026-08-30 23:04:21 +0700

    nhập n hiển thị ra n!

---
+++ src/n_giaithua.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class n_giaithua {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int answer = 1;
+        for (int i = 1; i <= n; i++) {
+            // answer *= i tương đương với answer = answer * i;
+            answer *= i;
+        }
+        System.out.print(answer);
+    }
+}
