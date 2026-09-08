de91cd9ee2f7f52de3100eb5cb517077a8ca438f
Vi Quang Huy <huyvq.b25cc084@stu.ptit.edu.vn>
2026-09-08 22:47:12 +0700

    replace

---
+++ src/replace.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class replace {
+    public static void main(String[] args) {
+        System.out.println("Cod3l3arn".replace('3', 'e'));
+        System.out.println("Blackcat".replace("Black", "White"));
+    }
+}
