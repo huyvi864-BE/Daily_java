b00edc3b720acb159c1644ad0c29c8f1844114cc
Vi Quang Huy <huyvq.b25cc084@stu.ptit.edu.vn>
2026-09-08 22:48:09 +0700

    index_Of

---
+++ src/indexof.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class indexof {
+    public static void main(String[] args) {
+        String s = "Codelearn";
+        System.out.println(s.indexOf("learn"));
+        System.out.println(s.indexOf("black"));
+    }
+}
